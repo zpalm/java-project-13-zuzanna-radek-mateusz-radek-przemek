cfbc8570ad20d8fc80abad4fc889f7d5cb127a89
mateusz58 <matp321@gmail.com>
2019-06-21 23:37:35 +0200

    Unit test correct

--- src/test/java/pl/coderstrust/HelloWorldTest.java
+++ src/test/java/pl/coderstrust/HelloWorldTest.java
@@ -2,10 +2,13 @@ package pl.coderstrust;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class HelloWorldTest {
 
     @Test
     public void shouldReturnTrueForPalindromeWord() {
+        assertTrue(HelloWorld.isPalindrome("abba"));
 
     }
 }
