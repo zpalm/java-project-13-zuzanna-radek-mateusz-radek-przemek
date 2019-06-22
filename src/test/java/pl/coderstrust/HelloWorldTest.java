91521a2985da972c8a05c07ae5b9685d703b5391
Radek87o <radoslaw.ornat@gmail.com>
2019-06-22 13:33:49 +0200

    added checkstyle and jacoco

--- src/test/java/pl/coderstrust/HelloWorldTest.java
+++ src/test/java/pl/coderstrust/HelloWorldTest.java
@@ -1,8 +1,8 @@
 package pl.coderstrust;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
 
 class HelloWorldTest {
 
