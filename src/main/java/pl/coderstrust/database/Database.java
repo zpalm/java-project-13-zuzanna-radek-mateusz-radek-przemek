025040509dc11162c5fd3e03c55b2b1941c5b97e
Przemek <Przemek@MacBook-Air-Przemek.local>
2019-06-23 18:02:11 +0200

    Database interface and exception fixes

--- src/main/java/pl/coderstrust/database/Database.java
+++ src/main/java/pl/coderstrust/database/Database.java
@@ -1,23 +1,24 @@
 package pl.coderstrust.database;
 
-import java.util.List;
+import java.util.Collection;
+import java.util.Optional;
 import pl.coderstrust.model.Invoice;
 
 public interface Database {
 
-    void save(Invoice invoice) throws DatabaseOperationException;
+    Invoice save(Invoice invoice) throws DatabaseOperationException;
 
-    void delete(Invoice invoice) throws DatabaseOperationException;
+    void delete(Long id) throws DatabaseOperationException;
 
-    Invoice getById(Long id) throws DatabaseOperationException;
+    Optional<Invoice> getById(Long id) throws DatabaseOperationException;
 
-    Invoice getByNumber(String number) throws DatabaseOperationException;
+    Optional<Invoice> getByNumber(String number) throws DatabaseOperationException;
 
-    List<Invoice> getAll() throws DatabaseOperationException;
+    Collection<Invoice> getAll() throws DatabaseOperationException;
 
     void deleteAll() throws DatabaseOperationException;
 
-    boolean exists(Invoice invoice) throws DatabaseOperationException;
+    boolean exists(Long id) throws DatabaseOperationException;
 
-    int count() throws DatabaseOperationException;
+    long count() throws DatabaseOperationException;
 }
--- src/main/java/pl/coderstrust/database/DatabaseOperationException.java
+++ src/main/java/pl/coderstrust/database/DatabaseOperationException.java
@@ -2,7 +2,19 @@ package pl.coderstrust.database;
 
 public class DatabaseOperationException extends Exception {
 
+    public DatabaseOperationException() {
+        super();
+    }
+
     public DatabaseOperationException(String message) {
         super(message);
+    }
+
+    public DatabaseOperationException(Throwable cause) {
+        super(cause);
+    }
+
+    public DatabaseOperationException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
