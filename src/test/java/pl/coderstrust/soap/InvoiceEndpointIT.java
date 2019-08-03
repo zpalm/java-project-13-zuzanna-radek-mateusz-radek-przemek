f0fc37b9bbb589eb1a6612fafa3e89fc9b7dab8a
Radek87o <radoslaw.ornat@gmail.com>
2019-08-03 12:26:02 +0200

    InvoiceEndpoinyIT modified

--- src/test/java/pl/coderstrust/soap/InvoiceEndpointIT.java
+++ src/test/java/pl/coderstrust/soap/InvoiceEndpointIT.java
@@ -3,14 +3,18 @@ package pl.coderstrust.soap;
 import static org.springframework.ws.test.server.RequestCreators.withPayload;
 import static org.springframework.ws.test.server.ResponseMatchers.noFault;
 import static org.springframework.ws.test.server.ResponseMatchers.payload;
-import static org.springframework.ws.test.server.ResponseMatchers.validPayload;
 
 import java.io.IOException;
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ClassPathResource;
@@ -20,7 +24,7 @@ import org.springframework.ws.test.server.MockWebServiceClient;
 
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-class InvoiceEndpointTest {
+class InvoiceEndpointIT {
 
     @Autowired
     private WebApplicationContext applicationContext;
@@ -33,100 +37,97 @@ class InvoiceEndpointTest {
         mockClient = MockWebServiceClient.createClient(applicationContext);
     }
 
-    @Test
+    @ParameterizedTest
+    @MethodSource("setOfRequestsAndResponses")
     @Order(1)
-    void shouldAddInvoice() throws IOException {
-        testComplianceOfRequestAndResponse("addInvoiceRequest", "addInvoiceResponse");
+    void shouldAddInvoice(String request, String response) throws IOException {
+        testComplianceOfRequestAndResponse(request, response);
+    }
+
+    private static Stream setOfRequestsAndResponses() {
+        return Stream.of(
+            Arguments.of("addInvoiceRequest_1", "addInvoiceResponse_1"),
+            Arguments.of("addInvoiceRequest_2", "addInvoiceResponse_2"),
+            Arguments.of("addInvoiceRequest_3", "addInvoiceResponse_5")
+        );
     }
 
     @Test
     @Order(2)
-    void shouldAddAnotherInvoice() throws IOException {
-        testComplianceOfRequestAndResponse("addAnotherInvoiceRequest", "addAnotherInvoiceResponse");
+    void shouldReturnStatusFailedDuringAddingInvoiceWhenInvoiceAlreadyExists() throws IOException {
+        testComplianceOfRequestAndResponse("addInvoiceRequest_2", "addInvoiceResponse_3");
     }
 
     @Test
     @Order(3)
-    void shouldAddInvoiceReturnStatusFailedWhenInvoiceAlreadyExists() throws IOException {
-        testComplianceOfRequestAndResponse("addAnotherInvoiceRequest", "addInvoiceOnceMoreResponse");
-    }
-
-    @Test
-    @Order(4)
     void shouldGetAllInvoices() throws IOException {
         testComplianceOfRequestAndResponse("getAllInvoicesRequest", "getAllInvoicesResponse");
     }
 
     @Test
-    @Order(5)
+    @Order(4)
     void shouldGetInvoiceById() throws IOException {
         testComplianceOfRequestAndResponse("getInvoiceByIdRequest", "getInvoiceByIdResponse");
     }
 
     @Test
-    @Order(6)
-    void shouldGetInvoiceByIdReturnFailedStatusWhenNonExistingInvoiceIsRetrieved() throws IOException {
+    @Order(5)
+    void shouldReturnStatusFailedDuringGettingInvoiceByIdWhenNonExistingInvoiceIsRetrieved() throws IOException {
         testComplianceOfRequestAndResponse("getNonExistingInvoiceByIdRequest", "getNonExistingInvoiceByIdResponse");
     }
 
     @Test
-    @Order(7)
+    @Order(6)
     void shouldUpdateInvoice() throws IOException {
         testComplianceOfRequestAndResponse("updateInvoiceRequest", "updateInvoiceResponse");
     }
 
     @Test
-    @Order(8)
-    void shouldUpdateInvoiceReturnFailedStatusWhenIdsAreNonCompliant() throws IOException {
-        testComplianceOfRequestAndResponse("updateInvoiceWithNonCompliantIdRequest", "updateInvoiceWithNonCompliantResponse");
+    @Order(7)
+    void shouldReturnStatusFailedDuringUpdatingInvoiceWhenIdsAreNonCompliant() throws IOException {
+        testComplianceOfRequestAndResponse("updateInvoiceWithNonCompliantIdRequest", "updateInvoiceWithNonCompliantIdResponse");
     }
 
     @Test
-    @Order(9)
-    void shouldUpdateInvoiceReturnFailedStatusWhenTryingToUpdateNonExistingInvoice() throws IOException {
+    @Order(8)
+    void shouldReturnStatusFailedDuringUpdatingInvoiceWhenTryingToUpdateNonExistingInvoice() throws IOException {
         testComplianceOfRequestAndResponse("updateNonExistingInvoiceRequest", "updateNonExistingInvoiceResponse");
     }
 
     @Test
-    @Order(10)
+    @Order(9)
     void shouldGetInvoiceByNumber() throws IOException {
         testComplianceOfRequestAndResponse("getInvoiceByNumberRequest", "getInvoiceByNumberResponse");
     }
 
     @Test
-    @Order(11)
-    void shouldGetInvoiceByNumberReturnFailedStatusTryingToGetNonExistingInvoice() throws IOException {
+    @Order(10)
+    void shouldReturnStatusFailedDuringGettingInvoiceByNumberWhenTryingToGetNonExistingInvoice() throws IOException {
         testComplianceOfRequestAndResponse("getInvoiceWithIncorrectNumberRequest", "getInvoiceWithIncorrectNumberResponse");
     }
 
     @Test
-    @Order(12)
+    @Order(11)
     void shouldDeleteInvoiceById() throws IOException {
         testComplianceOfRequestAndResponse("deleteInvoiceByIdRequest", "deleteInvoiceByIdResponse");
     }
 
     @Test
-    @Order(13)
-    void shouldDeleteInvoiceByIdReturnStatusFailedWhenInvoiceNotExists() throws IOException {
+    @Order(12)
+    void shouldReturnStatusFailedDuringDeletingInvoiceByIdWhenInvoiceNotExists() throws IOException {
         testComplianceOfRequestAndResponse("deleteInvoiceByIdRequest", "deleteNonExistingInvoiceByIdResponse");
     }
 
     @Test
-    @Order(14)
+    @Order(13)
     void shouldAddAnotherInvoiceAgain() throws IOException {
-        testComplianceOfRequestAndResponse("addAnotherInvoiceRequest", "addAnotherInvoiceAgainResponse");
+        testComplianceOfRequestAndResponse("addInvoiceRequest_2", "addInvoiceResponse_4");
     }
 
     @Test
-    @Order(15)
+    @Order(14)
     void shouldDeleteAllInvoices() throws IOException {
         testComplianceOfRequestAndResponse("deleteAllInvoicesRequest", "deleteAllInvoicesResponse");
-    }
-
-    @Test
-    @Order(16)
-    void shouldAddInvoiceWithoutIdSpecified() throws IOException {
-        testComplianceOfRequestAndResponse("addInvoiceWithoutIdRequest", "addInvoiceWithoutIdResponse");
     }
 
     private void testComplianceOfRequestAndResponse(String requestFile, String responseFile) throws IOException {
