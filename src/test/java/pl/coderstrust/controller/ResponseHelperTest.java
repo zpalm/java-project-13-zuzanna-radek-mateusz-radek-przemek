05cb4568067497e1ebf309db48ab9a7cb2e55c4c
Radek87o <radoslaw.ornat@gmail.com>
2019-08-22 18:35:10 +0200

    test

--- src/test/java/pl/coderstrust/controller/ResponseHelperTest.java
+++ src/test/java/pl/coderstrust/controller/ResponseHelperTest.java
@@ -1,8 +1,13 @@
 package pl.coderstrust.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
@@ -12,9 +17,62 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import pl.coderstrust.generators.InvoiceGenerator;
+import pl.coderstrust.model.Invoice;
+import pl.coderstrust.service.InvoicePdfService;
+import pl.coderstrust.service.ServiceOperationException;
 
 class ResponseHelperTest {
+
+    @Test
+    void shouldCreatePdfResponse() throws ServiceOperationException {
+        InvoicePdfService invoicePdfService = new InvoicePdfService();
+        byte[] bytes = invoicePdfService.createPdf(InvoiceGenerator.getRandomInvoice());
+
+        ResponseEntity<?> response = ResponseHelper.createPdfOkResponse(bytes);
+        assertEquals(bytes, response.getBody());
+    }
+
+    @Test
+    void shouldCreateJsonResponseWithOKStatus() {
+        Invoice randomInvoice = InvoiceGenerator.getRandomInvoice();
+        ResponseEntity<?> response = ResponseHelper.createJsonOkResponse(randomInvoice);
+        assertEquals(randomInvoice + "|" + MediaType.APPLICATION_JSON + "|" + HttpStatus.OK
+            , response.getBody() + "|" + response.getHeaders().getContentType() + "|" + response.getStatusCode());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenNullIsPassedAsArgument(){
+        Invoice randomInvoice = null;
+        ResponseEntity<?> response = ResponseHelper.createJsonOkResponse(randomInvoice);
+        assertEquals(randomInvoice + "|" + MediaType.APPLICATION_JSON + "|" + HttpStatus.OK
+            , response.getBody() + "|" + response.getHeaders().getContentType() + "|" + response.getStatusCode());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenUriHasIncorrectFormat(){
+        Invoice randomInvoice = InvoiceGenerator.getRandomInvoice();
+        String location = "!#FR@E$W$F@";
+        ResponseEntity<?> response = ResponseHelper.createJsonCreatedResponse(randomInvoice, location);
+        MediaType responseContentType = response.getHeaders().getContentType();
+        URI responseLocation = response.getHeaders().getLocation();
+        assertEquals(randomInvoice + "|" + MediaType.APPLICATION_JSON + "|" + HttpStatus.CREATED + "|" + location,
+            response.getBody() + "|" + responseContentType + "|" + response.getStatusCode() + "|" + responseLocation);
+    }
+
+    @Test
+    void shouldCreateJsonResponseWithCreatedStatus() {
+        Invoice randomInvoice = InvoiceGenerator.getRandomInvoice();
+        String location = "invoices/1";
+        ResponseEntity<?> response = ResponseHelper.createJsonCreatedResponse(randomInvoice, location);
+        MediaType responseContentType = response.getHeaders().getContentType();
+        URI responseLocation = response.getHeaders().getLocation();
+        assertEquals(randomInvoice + "|" + MediaType.APPLICATION_JSON + "|" + HttpStatus.CREATED + "|" + location,
+            response.getBody() + "|" + responseContentType + "|" + response.getStatusCode() + "|" + responseLocation);
+    }
 
     @Test
     void shouldReturnFalseWhenHttpHeadersAreEmpty() {
@@ -55,5 +113,20 @@ class ResponseHelperTest {
             Arguments.of(Arrays.asList(MediaType.APPLICATION_XML, MediaType.APPLICATION_PDF, MediaType.APPLICATION_JSON)),
             Arguments.of(Arrays.asList(MediaType.APPLICATION_PDF))
         );
+    }
+
+    private byte[] convertObjectToByteArray(Object object) throws IOException {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutputStream outputStream = null;
+        try {
+            outputStream = new ObjectOutputStream(bos);
+            outputStream.writeObject(object);
+            outputStream.flush();
+            return bos.toByteArray();
+        } catch (IOException e) {
+            throw new IllegalStateException("An Error occurred while converting object to byte array");
+        } finally {
+            bos.close();
+        }
     }
 }
